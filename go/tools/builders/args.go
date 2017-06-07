dbdfff6b95d157a59320169f3dc1853ff73818be
Ian Cottrell <iancottrell@google.com>
2017-06-07 18:49:21 -0400

    playing with struct flags

--- go/tools/builders/args.go
+++ go/tools/builders/args.go
@@ -19,9 +19,29 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"flag"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"reflect"
 	"strings"
+	"time"
 )
+
+func parseFlags(name string, flags interface{}, help string, args []string) ([]string, error) {
+	// Process the args
+	args, err := expandArgs(args)
+	if err != nil {
+		return args, err
+	}
+	// build a flag set that represents the struct
+	set := bindFlags(name, flags, help)
+	// parse the input with the flag set
+	if err := set.Parse(args); err != nil {
+		return args, err
+	}
+	return set.Args(), nil
+}
 
 // expandArgs finds any arguments starting with @ and replaces them with
 // the contents of the filename following the @.
@@ -34,7 +54,7 @@ func expandArgs(args []string) ([]string, error) {
 			continue
 		}
 		// We have a response file, so read it in now
-		content, err := ioutil.ReadFile(s)
+		content, err := ioutil.ReadFile(s[1:])
 		if err != nil {
 			return nil, err
 		}
@@ -44,4 +64,130 @@ func expandArgs(args []string) ([]string, error) {
 		}
 	}
 	return result, nil
+}
+
+// multiFlag allows repeated string flags to be collected into a slice
+type multiFlag struct {
+	values *[]string
+}
+
+func (m multiFlag) String() string {
+	if len(*m.values) == 0 {
+		return ""
+	}
+	return fmt.Sprint(*m.values)
+}
+
+func (m multiFlag) Set(v string) error {
+	(*m.values) = append(*m.values, v)
+	return nil
+}
+
+// bindFlags uses reflection to bind struct members to flag values.
+func bindFlags(name string, value interface{}, help string) *flag.FlagSet {
+	set := flag.NewFlagSet(name, flag.ExitOnError)
+	set.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", name)
+		fmt.Fprintln(os.Stderr, help)
+		set.PrintDefaults()
+	}
+	bindFlagEntry(set, name, value, help)
+	return set
+}
+
+// bindFlagEntry binds a sinle field entry to flags.
+// The field may be a struct of fields, or one of commonly understood types.
+// Unexported/unsettable fields, or any unknown types are ignored.
+func bindFlagEntry(set *flag.FlagSet, name string, value interface{}, help string) {
+	switch val := value.(type) {
+	case *bool:
+		set.BoolVar(val, name, *val, help)
+	case *int:
+		set.IntVar(val, name, *val, help)
+	case *int64:
+		set.Int64Var(val, name, *val, help)
+	case *uint:
+		set.UintVar(val, name, *val, help)
+	case *uint64:
+		set.Uint64Var(val, name, *val, help)
+	case *float64:
+		set.Float64Var(val, name, *val, help)
+	case *string:
+		set.StringVar(val, name, *val, help)
+	case *time.Duration:
+		set.DurationVar(val, name, *val, help)
+	case *[]string:
+		set.Var(multiFlag{val}, name, help)
+	case flag.Value:
+		set.Var(val, name, help)
+	default:
+		rv := reflect.ValueOf(value)
+		if rv.Kind() != reflect.Ptr || rv.Elem().Kind() != reflect.Struct {
+			return
+		}
+		e := rv.Elem()
+		t := e.Type()
+		for i := 0; i < e.NumField(); i++ {
+			tf := t.Field(i)
+			field := e.Field(i)
+			if !field.CanSet() {
+				continue // probably an unexported field
+			}
+			tags := tf.Tag
+			fname := strings.ToLower(tf.Name)
+			if explicitName := tags.Get("flag"); explicitName != "" {
+				fname = explicitName
+			}
+			bindFlagEntry(set, fname, field.Addr().Interface(), tags.Get("help"))
+		}
+	}
+}
+
+func emitFlags(value interface{}) []string {
+	result = []string{}
+	switch val := value.(type) {
+	case *bool:
+		if *val {
+			result = append(result, "-"+name)
+		}
+		set.BoolVar(val, name, *val, help)
+	case *int:
+		set.IntVar(val, name, *val, help)
+	case *int64:
+		set.Int64Var(val, name, *val, help)
+	case *uint:
+		set.UintVar(val, name, *val, help)
+	case *uint64:
+		set.Uint64Var(val, name, *val, help)
+	case *float64:
+		set.Float64Var(val, name, *val, help)
+	case *string:
+		set.StringVar(val, name, *val, help)
+	case *time.Duration:
+		set.DurationVar(val, name, *val, help)
+	case *[]string:
+		set.Var(multiFlag{val}, name, help)
+	case flag.Value:
+		set.Var(val, name, help)
+	default:
+		rv := reflect.ValueOf(value)
+		if rv.Kind() != reflect.Ptr || rv.Elem().Kind() != reflect.Struct {
+			return
+		}
+		e := rv.Elem()
+		t := e.Type()
+		for i := 0; i < e.NumField(); i++ {
+			tf := t.Field(i)
+			field := e.Field(i)
+			if !field.CanSet() {
+				continue // probably an unexported field
+			}
+			tags := tf.Tag
+			fname := strings.ToLower(tf.Name)
+			if explicitName := tags.Get("flag"); explicitName != "" {
+				fname = explicitName
+			}
+			bindFlagEntry(set, fname, field.Addr().Interface(), tags.Get("help"))
+		}
+	}
 }
--- go/tools/builders/filter_tags.go
+++ go/tools/builders/filter_tags.go
@@ -16,7 +16,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"go/build"
 	"log"
@@ -24,47 +23,46 @@ import (
 	"strings"
 )
 
+type FilterFlags struct {
+	Cgo    bool   `help:"Sets whether cgo-using files are allowed to pass the filter."`
+	Quiet  bool   `help:"Don't print filenames. Return code will be 0 if any files pass the filter"`
+	Tags   string `help:"Only pass through files that match these tags"`
+	Output string `help:"If set, write the matching files to this file, instead of stdout"`
+}
+
 func run(args []string) error {
 	// Prepare our flags
-	flags := flag.NewFlagSet("filter_tags", flag.ExitOnError)
-	cgo := flags.Bool("cgo", false, "Sets whether cgo-using files are allowed to pass the filter.")
-	quiet := flags.Bool("quiet", false, "Don't print filenames. Return code will be 0 if any files pass the filter.")
-	tags := flags.String("tags", "", "Only pass through files that match these tags.")
-	output := flags.String("output", "", "If set, write the matching files to this file, instead of stdout.")
-	// Process the args
-	args, err := expandArgs(args)
+	flags := FilterFlags{}
+	files, err := parseFlags("filter_tags", &flags, "filters filenames using go build tag logic", args)
 	if err != nil {
-		return err
-	}
-	if err := flags.Parse(args); err != nil {
 		return err
 	}
 	// filter our input file list
 	bctx := build.Default
-	bctx.CgoEnabled = *cgo
-	bctx.BuildTags = strings.Split(*tags, ",")
-	filenames, err := filterFiles(bctx, flags.Args())
+	bctx.CgoEnabled = flags.Cgo
+	bctx.BuildTags = strings.Split(flags.Tags, ",")
+	filtered, err := filterFiles(bctx, files)
 	if err != nil {
 		return err
 	}
-	// if we are in quite mode, just vary our exit condition based on the results
-	if *quiet {
-		if len(filenames) == 0 {
+	// if we are in quiet mode, just vary our exit condition based on the results
+	if flags.Quiet {
+		if len(filtered) == 0 {
 			os.Exit(1)
 		}
 		return nil
 	}
 	// print the outputs if we need not
 	to := os.Stdout
-	if *output != "" {
-		f, err := os.Create(*output)
+	if flags.Output != "" {
+		f, err := os.Create(flags.Output)
 		if err != nil {
 			return err
 		}
 		defer f.Close()
 		to = f
 	}
-	for _, filename := range filenames {
+	for _, filename := range filtered {
 		fmt.Fprintln(to, filename)
 	}
 
